0b638b830aab03e0b4de0b7d1e2a6390ea7cdf2a
Venkatakrishnan Shankar <venkatakrishnan.shankar@monsanto.com>
2016-11-11 20:30:54 -0600

    Adding model

--- src/main/java/com/sample/model/Todo.java
+++ src/main/java/com/sample/model/Todo.java
@@ -4,4 +4,40 @@ package com.sample.model;
  * Created by vshan on 11/11/16.
  */
 public class Todo {
+    private Long id;
+    private Long userId;
+    private String title;
+    private boolean completed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
