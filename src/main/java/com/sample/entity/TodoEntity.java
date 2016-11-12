1c6cb51fe0dfd1258f0bae222d9e1d761a6dec60
Venkatakrishnan Shankar <venkatakrishnan.shankar@monsanto.com>
2016-11-11 20:40:19 -0600

    Adding entity

--- src/main/java/com/sample/entity/TodoEntity.java
+++ src/main/java/com/sample/entity/TodoEntity.java
@@ -1,7 +1,59 @@
 package com.sample.entity;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
 /**
  * Created by vshan on 11/11/16.
  */
+@Entity
+@Table(name = "TODO")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Type(type = "true_false")
+    @Column(name = "COMPLETED")
+    private boolean completed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
